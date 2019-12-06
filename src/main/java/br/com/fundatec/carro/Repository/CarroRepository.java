87064487fd3d024566b35edd5c8db7e4fdbc04c0
FabioCostaTI <leleca2019>
2019-12-05 22:18:15 -0300

    Filtro por Data

---
+++ src/main/java/br/com/fundatec/carro/Repository/CarroRepository.java
@@ -0,0 +1,12 @@
+package br.com.fundatec.carro.Repository;
+
+import br.com.fundatec.carro.model.Carro;
+import org.springframework.data.repository.CrudRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface CarroRepository extends CrudRepository<Carro, Long> {
+    List<Carro> findByNomeContainingIgnoreCase (String nome);
+    List<Carro> findByDataFabricacaoBetween(LocalDate dataInicio, LocalDate dataFim);
+}
